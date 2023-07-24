0413a94e2f46bbc8b55fc6fc22b54a4faa6441d2
ALUMNO - ALEX CORDOVA HUARACHA <u19101608@utp.edu.pe>
2023-07-24 15:05:03 -0500

    2

--- src/redux/states/medicoSlice.js
+++ src/redux/states/medicoSlice.js
@@ -2,14 +2,14 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const DEFAULT_ESTADO_MEDICO =
 [{
-  id: "",
-  id_user: "",
-  masa_corporal: "",
-  fecha_nacimiento: "",
-  peso: "",
-  altura: "",
+  id: "1",
+  id_user: "00",
+  masa_corporal: "00",
+  fecha_nacimiento: "00",
+  peso: "00",
+  altura: "00",
   enfermedades: "Ninguna",
-  alergias: [],
+  alergias: ["ninguna"],
 }]
 
 let initialState = DEFAULT_ESTADO_MEDICO
