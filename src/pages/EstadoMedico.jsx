1789aed1c1c20ef858bd9fa4e9e18c3137269a33
ALUMNO - ALEX CORDOVA HUARACHA <u19101608@utp.edu.pe>
2023-07-24 15:24:35 -0500

    4

--- src/pages/EstadoMedico.jsx
+++ src/pages/EstadoMedico.jsx
@@ -5,9 +5,111 @@ import { useDispatch, useSelector } from "react-redux";
 import { createEstadoMedico } from "../redux/states/medicoSlice";
 import { useState } from "react";
 import { rebornPlato, selectPlatos } from "../redux/states/platosSlice";
-import { Enfermedades } from "../models/enfermedades";
 import ListaEstadosMedicos from "../components/ListaEstadosMedicos";
 import { useNavigate } from "react-router-dom";
+
+export const Enfermedades = [
+  {
+    enfermedad: "Ninguna",
+    ingredientesEvitar: ["minguno"],
+    ingredientesRecomendados: ["ninguno"]
+  },
+  {
+    enfermedad: "Hipertension",
+    ingredientesEvitar: ["Sal", "enpinacas", "zanahoria", "platano", "naranja"],
+    ingredientesRecomendados: ["Ajo", "Cebolla", "Aguacate", "Pescado", "Frutas","verdura"]
+  },
+  {
+    enfermedad: "Diabetes",
+    ingredientesEvitar: ["espinacas", "fresas", "pimientos", "Harina","manzana"],
+    ingredientesRecomendados: ["Vegetal de hojas verde", "Aguacate", "Nuez", "Pescado", "Carne"]
+  },
+  {
+    enfermedad: "Bronquitis",
+    ingredientesEvitar: ["queso","leche", "aji","rocoto","cafe"],
+    ingredientesRecomendados: ["limon","naranja", "Pescado", "Pollo", "Jengibre"]
+  },
+  {
+    enfermedad: "Gripe",
+    ingredientesEvitar: ["azucar", "refresco", "limon", "leche","naranja","salchichas"],
+    ingredientesRecomendados: ["limon","naranja", "Jengibre", "Caldo de pollo", "Ajo"]
+  },
+  {
+    enfermedad: "Dolor de garganta",
+    ingredientesEvitar: ["aji", "tomate", "zanahoria", "limon", "naranja"],
+    ingredientesRecomendados: ["Sopa", "papas", "Te", "Miel"]
+  },
+  {
+    enfermedad: "Cardiovasculares",
+    ingredientesEvitar: ["carne", "lacteo", "fritura", "Sal", "Azucar"],
+    ingredientesRecomendados: ["Pescado", "Aguacate", "Nuez", "Aceite de oliva", "Fruta","verdura"]
+  },
+  {
+    enfermedad: "Enfermedad celiaca",
+    ingredientesEvitar: ["Trigo", "Cebada", "Centeno", "Pan", "Pasta", "Galleta", "gluten"],
+    ingredientesRecomendados: ["Arroz", "papas", "Quinoa", "Verdura", "Fruta", "Carne"]
+  },
+  {
+    enfermedad: "Intolerancia a la lactosa",
+    ingredientesEvitar: ["Leche", "Queso", "Yogur", "Helado", "Mantequilla"],
+    ingredientesRecomendados: ["Leche sin lactosa", "Leche vegetal", "Yogur sin lactosa", "Mantequilla sin lactosa", "Queso sin lactosa"]
+  },
+  {
+    enfermedad: "Enfermedad renal cronica",
+    ingredientesEvitar: ["Sal", "Platano", "Naranja", "Espinacas", "Aguacate", "Alimento rico en potasio"],
+    ingredientesRecomendados: ["Manzana", "Pera", "Ciruela", "Arandano", "Aguacate", "Vegetal","palta"]
+  },
+  {
+    enfermedad: "Enfermedad de Crohn",
+    ingredientesEvitar: ["Nuez", "coliflor", "Lacteo", "picante", "brocoli","lenteja","aji","carne"],
+    ingredientesRecomendados: ["Pescado", "Huevos", "Arroz", "Pasta blanca", "Calabaza", "papas"]
+  },
+  {
+    enfermedad: "Colitis ulcerosa",
+    ingredientesEvitar: ["leche", "queso", "lenteja", "cebolla", "limon"],
+    ingredientesRecomendados: ["Pescado", "Carne", "Verdura", "Fruta", "Calabaza"]
+  },
+  {
+    enfermedad: "Anemia",
+    ingredientesEvitar: ["Carne", "ajo", "espinacas", "huevos", "uvas","leche"],
+    ingredientesRecomendados: ["Carne", "Pollo", "Pavo", "Pescado", "Espinaca", "Lenteja", "Huevos"]
+  },
+  {
+    enfermedad: "Enfermedad de Hashimoto",
+    ingredientesEvitar: ["Soja", "queso", "Brocoli", "nuez", "Coliflor", "Frijol"],
+    ingredientesRecomendados: ["Pescado", "Huevos", "Arroz", "papas", "Fruta","verdura", "Aceite de oliva"]
+  },
+  {
+    enfermedad: "Osteoporosis",
+    ingredientesEvitar: ["Cafe", "sal", "espinacas","naranja"],
+    ingredientesRecomendados: ["Leche", "Yogur", "Queso", "Salmon", "Brocoli", "Nuez", "Almendra"]
+  },
+  {
+    enfermedad: "Gota",
+    ingredientesEvitar: ["Marisco", "Viscera", "Carne", "Esparrago", "Coliflor", "Legumbre"],
+    ingredientesRecomendados: ["Cereza", "Fresa", "Arandano", "Vegetal de hoja verde", "Pescado", "Huevos"]
+  },
+  {
+    enfermedad: "Enfermedad hepatica",
+    ingredientesEvitar: ["salchichas", "Grasa", "galletas", "Azucar","queso"],
+    ingredientesRecomendados: ["Fruta y verdura fresca", "Pescado", "Huevos", "Aceite de oliva", "Cereal integral"]
+  },
+  {
+    enfermedad: "Enfermedad pulmonar",
+    ingredientesEvitar: ["coliflor", "fritura", "Embutido", "brocoli","atun"],
+    ingredientesRecomendados: ["Pescado", "Fruta y verdura fresca", "Fruto seco", "Aceite de oliva", "Agua"]
+  },
+  {
+    enfermedad: "Enfermedad de Alzheimer",
+    ingredientesEvitar: ["mantequilla", "galletas", "pan", "carne"],
+    ingredientesRecomendados: ["Pescado", "Nuez", "Fruta y verdura fresca", "Aceite de oliva", "Cereal integral"]
+  },
+  {
+    enfermedad: "Sindrome del intestino irritable (SII)",
+    ingredientesEvitar: ["Cebolla", "Ajo", "Brocoli", "Repollo", "Legumbre", "Fritura"],
+    ingredientesRecomendados: ["Pescado", "Pollo", "Arroz", "Zanahoria", "Calabaza", "papas"]
+  }
+];
 
 const EstadoMedico = () => {
   const navigate = useNavigate();
